5c405074cf990b873238d8e7e3ccd18e67f3724c
Matheus Silveira Venturini <msventurini@icloud.com>
2025-05-27 21:27:43 -0300

    WIP: Refactoring properties to animatable data.

--- Sources/GameboyTransition/Model/HandheldGameConsoleData/HandheldGameConsole.swift
+++ Sources/GameboyTransition/Model/HandheldGameConsoleData/HandheldGameConsole.swift
@@ -16,7 +16,6 @@ public enum HandheldGameConsole: String, DataGroupContainer {
         return self.description
     }
     
-    
     case gameboyDMG
     case gameboyAdvance
     case nintendoSwitch
@@ -33,13 +32,6 @@ public enum HandheldGameConsole: String, DataGroupContainer {
         return .init(console: self)
     }
 
-    //USAR ANY VAI SER BUG NO FUTURO, RESOLVER
-    //    func styleDataFor(component: ConsoleSection) -> ComponentStyleData {
-    func styleDataFor(component: ConsoleSection) -> any ComponentStyleData {
-        return component.getStyleData(container: self)
-    }
-    
-    
     var handheldLayout: HandheldLayout {
         switch self {
         case .gameboyDMG:
@@ -51,17 +43,6 @@ public enum HandheldGameConsole: String, DataGroupContainer {
         }
     }
     
-    var size: AnimatableSize {
-        switch self {
-        case .gameboyDMG:
-            return .init(size: .init(width: 90, height: 148))
-        case .gameboyAdvance:
-            return .init(size: .init(width: 144, height: 82))
-        case .nintendoSwitch:
-            return .init(size: .init(width: 144, height: 82))
-        }
-    }
-    
     var sections: [ConsoleSection] {
         switch self {
         case .gameboyDMG:
@@ -70,60 +51,6 @@ public enum HandheldGameConsole: String, DataGroupContainer {
             return [.consoleScreen, .controllerLeft, .controllerRight]
         case .nintendoSwitch:
             return [.consoleScreen, .controllerLeft, .controllerRight]
-        }
-    }
-    
-    var widthRatio: Double {
-        
-        guard isSizeNotZeroOrNegative && isWidthLessThanHeight else {
-            return 1
-        }
-        
-        return widthSize / heightSize
-        
-    }
-    
-    var heightRatio: Double {
-        
-        guard isSizeNotZeroOrNegative && isHeightLessThanWidth else {
-            return 1
-        }
-        
-        return heightSize / widthSize
-        
-    }
-    
-    var isSizeNotZeroOrNegative: Bool {
-        return (widthSize > 0) && (heightSize > 0)
-    }
-    
-    var isWidthLessThanHeight: Bool {
-        return (widthSize < heightSize)
-    }
-    
-    var isHeightLessThanWidth: Bool {
-        return (widthSize < heightSize)
-    }
-    
-    var widthSize: Double {
-        switch self {
-        case .gameboyDMG:
-            return 90
-        case .gameboyAdvance:
-            return 10
-        case .nintendoSwitch:
-            return 10
-        }
-    }
-    
-    var heightSize: Double {
-        switch self {
-        case .gameboyDMG:
-            return 148
-        case .gameboyAdvance:
-            return 10
-        case .nintendoSwitch:
-            return 10
         }
     }
 }
